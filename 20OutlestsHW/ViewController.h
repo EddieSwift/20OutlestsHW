091ec3f3bd4bda1deb930d0613b400b2bf1b8284
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2018-09-25 17:57:22 +0300

    All levels

--- 20OutlestsHW/ViewController.h
+++ 20OutlestsHW/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIView *chessBoard;
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *whiteCellsCollection;
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *allCheckers;
 
 @end
 
--- 20OutlestsHW/ViewController.m
+++ 20OutlestsHW/ViewController.m
@@ -19,5 +19,36 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (CGFloat) calculatingRandColors {
+    
+    return (float) (arc4random() % 256) / 255;
+}
+
+- (UIColor*) randomColor {
+    
+    CGFloat r = [self calculatingRandColors];
+    CGFloat g = [self calculatingRandColors];
+    CGFloat b = [self calculatingRandColors];
+    
+    return [UIColor colorWithRed:r green:g blue:b alpha:1];
+}
+
+- (UIColor*) shuffleCheckers {
+    
+    NSInteger random = arc4random() % 2;
+    
+    return random ? [UIColor whiteColor] : [UIColor redColor];
+}
+
+- (void) viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    
+    self.chessBoard.backgroundColor = [self randomColor];
+
+    for (UIView *views in self.allCheckers) {
+        
+        views.backgroundColor = [self shuffleCheckers];
+    }
+
+}
 
 @end
