64ec12bcfc65425addc84bb1e04f499d7f116c2a
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2018-09-25 09:28:24 +0300

    Initial Commit

---
+++ 20OutlestsHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20OutlestsHW
+//
+//  Created by Eduard Galchenko on 9/25/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20OutlestsHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20OutlestsHW
+//
+//  Created by Eduard Galchenko on 9/25/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
